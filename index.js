466a37c0a2f29f5a053ca017bfef4b0091453d75
Michael T <Michael@Mikes-Air.home>
2019-07-11 12:03:14 -0400

    even better

--- index.js
+++ index.js
@@ -2,4 +2,16 @@ $(document).ready(function() {
     $(".right").scroll(function(){
         $("#chevron").css("opacity", 1 - $(".right").scrollTop() / 500);
     })
+    $(window).on("resize", function (e) {
+      checkScreenSize();
+  });
+
+  checkScreenSize();
+
+  function checkScreenSize(){
+      var newWindowWidth = $(window).width();
+      if (newWindowWidth < 600) {
+          
+      }
+    }
 })
